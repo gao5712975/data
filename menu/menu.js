ce6e44cf52b57497619f6f334e130f7bea5b4fd2
moka <gaoyuanfell@sina.com>
2016-09-29 00:08:40 +0800

    1

--- menu/menu.js
+++ menu/menu.js
@@ -4,131 +4,187 @@ app.controller('menuCtrl',['$scope',function(){
 
 }])
 
-
-!function(win){
+// window hash标志 iframe ID
+!function(win,key,frame){
     function Route(){
         this.regex = {};
-        win.top.onhashchange = function(event){
-            console.info(event)
-        }
+        this.data = {};
     }
 
-    Route.getHash = function(){
-        return 
-    }
+    var _self = win.top.$R = new Route();
+
+    Route.prototype.changeState = function (hash) {
+        if(hash){
+            var url = this.getUrl(hash);
+            document.getElementById(frame).setAttribute('src',url);
+            return true;
+        }
+    };
+
+    Route.prototype.getHash = function(){
+        var r = '#' + key;
+        return location.hash.substr(r.length + 1);
+    };
+
+    Route.prototype.getUrl = function (hash) {
+        return this.regex[hash]
+    };
 
     //name url 
     Route.prototype.reg = function(o){
-        if(!o.name || !o.url) return;
-        this.regex[o.name] = o.url;
+        if(o instanceof Array){
+            for(var i = 0,j = o.length;i<j;i++){
+                var r = o[i];
+                if(!r.name || !r.url) continue;
+                this.regex[r.name] = r.url;
+            }
+        }else{
+            if(!o.name || !o.url) return this;
+            this.regex[o.name] = o.url;
+        }
         return this;
-    }
+    };
 
-    this.$R = new Route();
-}(this)
+    Route.prototype.listen = function () {
+        win.top.onhashchange = function(event){
+            var hash = _self.getHash();
+            if(_self.getUrl(hash)){
+                _self.changeState(hash);
+            }
+        }
+    };
+
+    Route.prototype.start = function (parent) {
+        var a;
+        if(parent){
+            a = win.top.document.querySelectorAll(parent + ' a[data-route]');
+        }else{
+            a = win.top.document.querySelectorAll('a[data-route]');
+        }
+        if(a && a.length > 0){
+            for(var i = 0,j = a.length;i<j;i++){
+                var _a = a[i];
+                var r = _a.getAttribute('data-route');
+                _a.setAttribute('href','#' + key + '/' + r);
+            }
+        }
+        var hash = _self.getHash();
+        if(_self.getUrl(hash)){
+            _self.changeState(hash);
+        }
+        _self.listen();
+    };
+
+    Route.prototype.go = function (hash, data) {
+        if(_self.getUrl(hash)){
+            top.location.hash = '#' + key + '/' + hash;
+            _self.data = data;
+        }
+    }
+}(this,'!','iframe');
 
 
 //全局hash路由控制
-// var route = {
-       
-//     start: function () {
-//         this.match(top.location.hash);
-//         this.listen();
-//     },
-//     url: function (url) {
-//         var tdoc = $(top.document.body),
-//             ifr = tdoc.find("#main"),
-//             menu = (url.match(this.routeReg) || [])[1];
+var route = {
+    search:{},
+    start: function () {
+        this.match(top.location.hash);
+        this.listen();
+    },
+    url: function (url) {
+        var tdoc = $(top.document.body),
+            ifr = tdoc.find("#main"),
+            menu = (url.match(this.routeReg) || [])[1];
 
-//         ifr && (tdoc.find(".menu li a").removeClass("current"), tdoc.find(".menu li[data-menu=" + menu + "] a").addClass("current"), ifr.attr("src", url.slice(1)));
-//     },
-//     routeReg: /^#?(ADManage|SystemManage|ResourceManage|ReportManage|ClientManage)/i,
-//     match: function (url) {
-//         this.routeReg.test(url) && this.url(url);
-//     },
-//     listen: function () {
-//         var that = this;
-//         top.onhashchange = function (event) {
-//             that.match(top.location.hash);
-//         }
-//     },
-//     change: function (url) {
-//         this.routeReg.test(url) && (top.location.hash = url);
-//     }
-// };
+        ifr && (tdoc.find(".menu li a").removeClass("current"), tdoc.find(".menu li[data-menu=" + menu + "] a").addClass("current"), ifr.attr("src", url.slice(1)));
+    },
+    routeReg: /^#?(ADManage|SystemManage|ResourceManage|ReportManage|ClientManage)/i,
+    match: function (url) {
+        this.routeReg.test(url) && this.url(url);
+    },
+    listen: function () {
+        var that = this;
+        top.onhashchange = function (event) {
+            that.match(top.location.hash);
+        }
+    },
+    change: function (url,data) {
+        this.routeReg.test(url) && (top.location.hash = url,this.search = data);
+    }
+};
+/*
+var Q = function (W, D, M, HTML, hash, view, arg, LL, i, index, Regex, Q) {
+    HTML = D.getElementsByTagName('html')[0];
+    Regex = [];
+    onhashchange = function () {
+        Q.hash = hash = location.hash;
 
-// var Q = function (W, D, M, HTML, hash, view, arg, LL, i, index, Regex, Q) {
-//     HTML = D.getElementsByTagName('html')[0];
-//     Regex = [];
-//     onhashchange = function () {
-//         Q.hash = hash = location.hash;
+        arg = hash.split('/');
 
-//         arg = hash.split('/');
+        i = Regex.length;
+        while (i--)
+            if (LL = hash.match(Regex[i])) {
+                arg[0] = Regex[i];
+                break;
+            }
 
-//         i = Regex.length;
-//         while (i--)
-//             if (LL = hash.match(Regex[i])) {
-//                 arg[0] = Regex[i];
-//                 break;
-//             }
+        if (!Q[arg[0]])
+            arg[0] = index;
 
-//         if (!Q[arg[0]])
-//             arg[0] = index;
+        if (Q.pop)
+            Q.pop.apply(W, arg);
 
-//         if (Q.pop)
-//             Q.pop.apply(W, arg);
+        Q.lash = view = arg.shift();
 
-//         Q.lash = view = arg.shift();
+        HTML.setAttribute('view', view);
 
-//         HTML.setAttribute('view', view);
+        Q[view].apply(W, arg);
+    };
+    Q = {
+        init: function (o) {
 
-//         Q[view].apply(W, arg);
-//     };
-//     Q = {
-//         init: function (o) {
+            index = o.index || 'V';
 
-//             index = o.index || 'V';
+            if (o.pop && typeof o.pop == 'function')
+                Q.pop = o.pop;
 
-//             if (o.pop && typeof o.pop == 'function')
-//                 Q.pop = o.pop;
+            onhashchange();
 
-//             onhashchange();
+            return this
+        },
+        reg: function (r, u) {
+            if (!r)
+                return;
 
-//             return this
-//         },
-//         reg: function (r, u) {
-//             if (!r)
-//                 return;
+            if (u == undefined)
+                u = function () {};
 
-//             if (u == undefined)
-//                 u = function () {};
+            if (r instanceof RegExp) {
+                Q[r] = u;
+                Regex.push(r);
+            } else if (r instanceof Array) { //数组注册
+                for (var i in r) {
+                    L = [].concat(r[i]).concat(u);
+                    this.reg.apply(this, L);
+                }
+            } else if (typeof r == 'string') {
+                if (typeof u == 'function')
+                    Q[r] = u;
+                else if (typeof u == 'string' && Q[u])
+                    Q[r] = Q[u];
+            }
 
-//             if (r instanceof RegExp) {
-//                 Q[r] = u;
-//                 Regex.push(r);
-//             } else if (r instanceof Array) { //数组注册
-//                 for (var i in r) {
-//                     L = [].concat(r[i]).concat(u);
-//                     this.reg.apply(this, L);
-//                 }
-//             } else if (typeof r == 'string') {
-//                 if (typeof u == 'function')
-//                     Q[r] = u;
-//                 else if (typeof u == 'string' && Q[u])
-//                     Q[r] = Q[u];
-//             }
-
-//             return this
-//         },
-//         V: function () {
-//             console.log('Q.js <https://github.com/itorr/q.js> 2014/12/28');
-//             return this
-//         },
-//         go: function (u) {
-//             location.hash = '#' + u;
-//             return this
-//         }
-//     };
-//     return Q;
-// }(this, document);
+            return this
+        },
+        V: function () {
+            console.log('Q.js <https://github.com/itorr/q.js> 2014/12/28');
+            return this
+        },
+        go: function (u) {
+            location.hash = '#' + u;
+            return this
+        }
+    };
+    return Q;
+}(this, document);*/
 
